856cca3cf8e3d7f8d83c25af30ae8a2dee6d70b0
Andrew Osborn <osbornandrewj@gmail.com>
2021-10-19 11:17:04 -0700

    commit tree 4794d5ad1fd2af49c8433f9ef3227803f8d70901

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const alexanderHamilton = document.getElementById('main');
+alexanderHamilton.remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+victory.innerHTML = '<h1> AJ is the champion </h1>';
